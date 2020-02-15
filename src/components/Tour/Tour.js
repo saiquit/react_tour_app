fb0d0c6b333ee76017b30542e75d163600540af0
saiquit <imamhossain130754@gmail.com>
2020-02-15 22:59:17 +0600

    2nd

---
+++ src/components/Tour/Tour.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import "./Tour.scss";
+
+export default class Tour extends Component {
+  state = {
+    showInfo: false
+  };
+  handleInfo = () => {
+    this.setState({
+      showInfo: !this.state.showInfo
+    });
+  };
+
+  render() {
+    const { id, city, img, name, info } = this.props.tour;
+    const { removeTour } = this.props;
+
+    return (
+      <article className="tour">
+        <div className="img-container">
+          <img src={img} alt="" />
+          <span className="close-btn">
+            <i
+              className="fa fa-window-close"
+              onClick={() => {
+                removeTour(id);
+              }}
+            ></i>
+          </span>
+        </div>
+        <div className="tour-info">
+          <h3>{city}</h3>
+          <h4>{name}</h4>
+          <h5>
+            info{" "}
+            <span>
+              <i
+                className="fa fa-arrow-circle-down"
+                onClick={this.handleInfo}
+              ></i>
+            </span>
+          </h5>
+          {this.state.showInfo && <p>{info}</p>}
+        </div>
+      </article>
+    );
+  }
+}
